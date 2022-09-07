e21b7e83670351d86c46864dec727a2ab35a31ef
Manas Saxena <manassaxena160601@gmail.com>
2022-09-07 22:53:54 +0530

    removed production error

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
   __dirname = path.resolve();
   
   if(process.env.NODE_ENV == 'production' ){
-    app.use(express.static(path.join(__dirname, "/client/build")))
+    app.use(express.static(path.join(__dirname, "client/build")))
     app.use((req, res, next) => {
       res.sendFile(path.join(__dirname,"client", "build", "index.html"));
     });
